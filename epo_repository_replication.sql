9d43bd781fcd6197d53ea343f7b30337b7d344cf
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2023-11-10 23:49:52 +0200

    Update epo_repository_replication.sql

--- epo_repository_replication.sql
+++ epo_repository_replication.sql
@@ -6,8 +6,11 @@ SELECT
 	[EPORepositoryStatus].[lastreplication] AS [last_replication],
 CASE
 	WHEN [EPORepositoryStatus].[status] = 0 THEN 'Needs first update'
+	WHEN [EPORepositoryStatus].[status] = 1 THEN 'Repository busy'
 	WHEN [EPORepositoryStatus].[status] = 2 THEN 'Last replication failed'
+	WHEN [EPORepositoryStatus].[status] = 3 THEN 'Last replication succeeded'
 	WHEN [EPORepositoryStatus].[status] = 4 THEN 'Main Repository idle'
+	WHEN [EPORepositoryStatus].[status] = 5 THEN 'Main Repository corrupt'
     ELSE 'unknown'
 END AS [status]
 FROM [EPORepositoryStatus]
